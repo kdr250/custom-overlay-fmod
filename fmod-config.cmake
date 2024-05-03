8effdfb736e90ab276df68a00fdeeaf3a983bbb3
kdr250 <54400975+kdr250@users.noreply.github.com>
2024-05-03 18:35:33 +0900

    DebugとReleaseを指定

--- fmod-config.cmake
+++ fmod-config.cmake
@@ -3,13 +3,22 @@ get_filename_component(FMOD_BASE_DIR "${CMAKE_CURRENT_LIST_DIR}/../../" ABSOLUTE
 
 # add info about location of the dynamic library file
 if (WIN32)
-    cmake_path(SET FMOD_DLL_PATH NORMALIZE ${FMOD_BASE_DIR}/bin/fmod.dll)
-    cmake_path(SET FMOD_LIB_PATH NORMALIZE ${FMOD_BASE_DIR}/lib/fmod_vc.lib)
+    cmake_path(SET FMOD_DLL_PATH_DEBUG NORMALIZE ${FMOD_BASE_DIR}/debug/bin/fmod.dll)
+    cmake_path(SET FMOD_LIB_PATH_DEBUG NORMALIZE ${FMOD_BASE_DIR}/debug/lib/fmod_vc.lib)
     set_target_properties(
         FMOD::FMOD PROPERTIES
-        IMPORTED_LOCATION "${FMOD_DLL_PATH}"
-        IMPORTED_IMPLIB "${FMOD_LIB_PATH}"
+        IMPORTED_LOCATION_DEBUG "${FMOD_DLL_PATH_DEBUG}"
+        IMPORTED_IMPLIB_DEBUG "${FMOD_LIB_PATH_DEBUG}"
     )
+
+    cmake_path(SET FMOD_DLL_PATH_RELEASE NORMALIZE ${FMOD_BASE_DIR}/bin/fmod.dll)
+    cmake_path(SET FMOD_LIB_PATH_RELEASE NORMALIZE ${FMOD_BASE_DIR}/lib/fmod_vc.lib)
+    set_target_properties(
+        FMOD::FMOD PROPERTIES
+        IMPORTED_LOCATION_RELEASE "${FMOD_DLL_PATH_RELEASE}"
+        IMPORTED_IMPLIB_RELEASE "${FMOD_LIB_PATH_RELEASE}"
+    )
+
 elseif (APPLE)
     set_target_properties(
         FMOD::FMOD PROPERTIES
